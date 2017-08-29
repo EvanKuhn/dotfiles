4dac35f36b0f9ac5f16180620979e83e27005c6b
Evan Kuhn <ekuhn@yahoo-inc.com>
2017-08-29 14:58:41 -0700

    update csshxify

--- utils/csshxify.rb
+++ utils/csshxify.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/env ruby
 # This script takes in a bunch of hostnames, one per line, and runs csshX
 # on all of them.
+require 'optparse'
+
+screen = 1
+OptionParser.new do |opts|
+  opts.on("--screen NUM", "Screen number") { |v| screen = Integer(v) }
+end.parse!
+
 input = $stdin.read
 hosts = input.split("\n")
-command = "csshX \\\n" + hosts.join(" \\\n")
+command = "csshX --screen #{screen} \\\n" + hosts.join(" \\\n")
 puts command
 system(command)
